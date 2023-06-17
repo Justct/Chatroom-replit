758befe6eb7536a0788a44c205319b927a122f41
Cactochan <85914469+merwin-asm@users.noreply.github.com>
2023-06-17 18:14:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import platform
 room_name = input("Room Name  : ")
 room_url = input("Room URL (https://<replit project name>.<acc name>.repl.co) : ")
 
-f = open("info.json","w")
+f = open("Chatroom-replit/info.json","w")
 f.write(json.dumps({"name":room_name, "url" : room_url}))
 f.close()
 
@@ -15,18 +15,18 @@ def P():
     return response.text == room_url
 
 if P():
-  os.system(f"python3 api.py 0.0.0.0 8000")
+  os.system(f"python3 Chatroom-replit/api.py 0.0.0.0 8000")
 
 
 def start_server(ip, port):
     if not P():
         if platform.system().lower() == "linux":
-            os.system(f"python3 verify.py {ip} {port}")
+            os.system(f"python3 Chatroom-replit/verify.py {ip} {port}")
         else:
-            os.system(f"python verify.py {ip} {port}")      
+            os.system(f"python Chatroom-replit/verify.py {ip} {port}")      
 
 
-    os.system(f"python3 api.py {ip} {port}")
+    os.system(f"python3 Chatroom-replit/api.py {ip} {port}")
 
 def main():
     start_server("0.0.0.0", 8000)
