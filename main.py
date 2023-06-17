432d5351a6d5c0ca36e53cf2ab25006ae14d7da4
Cactochan <85914469+merwin-asm@users.noreply.github.com>
2023-06-17 17:42:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,17 +14,17 @@ def P():
     response = requests.get(f"https://backendjustchat.darkmash.repl.co/find/{room_name}")
     return response.text == room_url
 
+if P():
+  os.system(f"python3 api.py 0.0.0.0 8000")
+
+
 def start_server(ip, port):
     if not P():
         if platform.system().lower() == "linux":
             os.system(f"python3 verify.py {ip} {port}")
         else:
             os.system(f"python verify.py {ip} {port}")      
-    print("WAIT 10 SECS")
-    time.sleep(10)
-    if not P():
-        print("The room url and the url provided didnt match or someother error!! Quiting")
-        quit()
+
 
     os.system(f"python3 api.py {ip} {port}")
 
