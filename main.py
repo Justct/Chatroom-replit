591f77f38ce726af02e8e59d0c3d1b82d18c20a9
Cactochan <85914469+merwin-asm@users.noreply.github.com>
2023-06-17 11:53:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 import os
+import requests
+import json
+
+room_name = input("Room Name  : ")
+room_url = input("Room URL : ")
+
+f = open("info.json","w")
+f.write(json.dumps({"name":room_name, "url" : room_url}))
+f.close()
+
+def P():
+    response = requests.get(f"https://backendjustchat.darkmash.repl.co/find/{room_name}")
+    return response.text == room_url
 
 def start_server(ip, port):
-    os.system(f"python3 Chatroom/api.py {ip} {port}")
+    if not P():
+        if platform.system().lower() == "linux":
+            os.system(f"python3 verify.py {ip} {port}")
+        else:
+            os.system(f"python verify.py {ip} {port}")      
+    print("WAIT 10 SECS")
+    time.sleep(10)
+    if not P():
+        print("The room url and the url provided didnt match or someother error!! Quiting")
+        quit()
+
+    os.system(f"python3 api.py {ip} {port}")
 
 def main():
     start_server("0.0.0.0", 8000)
